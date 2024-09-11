1e0f90147933c5c4eb07886769f6291e010796af
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-09-11 13:09:02 -0700

    Update script.js

--- milestone-3/script.js
+++ milestone-3/script.js
@@ -17,7 +17,8 @@ var _a;
         var experience = experienceelement.value;
         var skill = skillselement.value;
         //create resume output
-        var resumeoutput = "\n<h2>Resume</h2>\n<P><strong>Name:</strong> ".concat(name_1, " </p>\n<P><strong>email:</strong>").concat(email, "</p>\n<P><strong>Phone Number:</strong>").concat(phone, "</p>\n\n<h3>Education</h3>\n<P>").concat(education, "</p>\n\n<h3>Skills</h3>\n<P>").concat(skill, "</p>\n");
+        var resumeoutput = "\n<h2>Resume</h2>\n<P><strong>Name:</strong> <spam id=\"edit-name\" class=\"editable\"> ".concat(name_1, "</spam> </p>\n<P><strong>email:</strong> <spam id=\"edit-email\" class=\"editable\">").concat(email, "</spam></p>\n<P><strong>Phone Number:</strong><spam id=\"edit-phone\" class=\"editable\">").concat(phone, "</spam></p>\n\n<h3>Education</h3>\n<P>").concat(education, "</p>\n\n\n<h3>Experience</h3>\n<P >").concat(experience, "</p>\n<h3>Skills</h3>\n<P >").concat(skill, "</p>\n");
+        
         var resumeoutputelement = document.getElementById("resumeoutput");
         if (resumeoutputelement) {
             resumeoutputelement.innerHTML = resumeoutput;
