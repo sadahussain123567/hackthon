241b0a0ea4ab0c2381d6b8059dc62a97ceafb589
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-09-11 12:22:32 -0700

    Add files via upload

---
+++ milestone-3/script.js
@@ -0,0 +1,29 @@
+var _a;
+//Listing Element 
+(_a = document.getElementById("resumeform")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (event) {
+    event.preventDefault();
+    //Type Assertion
+    var nameelement = document.getElementById("name");
+    var emailelement = document.getElementById("email");
+    var phoneelement = document.getElementById("phone");
+    var educationelement = document.getElementById("education");
+    var experienceelement = document.getElementById("experience");
+    var skillselement = document.getElementById("skills");
+    if (nameelement && emailelement && phoneelement && experienceelement && skillselement && educationelement) {
+        var name_1 = nameelement.value;
+        var email = emailelement.value;
+        var phone = phoneelement.value;
+        var education = educationelement.value;
+        var experience = experienceelement.value;
+        var skill = skillselement.value;
+        //create resume output
+        var resumeoutput = "\n<h2>Resume</h2>\n<P><strong>Name:</strong> ".concat(name_1, " </p>\n<P><strong>email:</strong>").concat(email, "</p>\n<P><strong>Phone Number:</strong>").concat(phone, "</p>\n\n<h3>Education</h3>\n<P>").concat(education, "</p>\n\n<h3>Skills</h3>\n<P>").concat(skill, "</p>\n");
+        var resumeoutputelement = document.getElementById("resumeoutput");
+        if (resumeoutputelement) {
+            resumeoutputelement.innerHTML = resumeoutput;
+        }
+        else {
+            console.error("The Resume Output Is Missing");
+        }
+    }
+});
---
+++ milestone-3/script.ts
@@ -0,0 +1,44 @@
+//Listing Element 
+document.getElementById(`resumeform`)?.addEventListener(`submit`, function (event) {
+  event.preventDefault();
+
+  //Type Assertion
+  const nameelement = document.getElementById(`name`) as HTMLInputElement;
+  const emailelement = document.getElementById(`email`) as HTMLInputElement;
+  const phoneelement = document.getElementById(`phone`) as HTMLInputElement;
+  const educationelement = document.getElementById(`education`) as HTMLInputElement;
+  const experienceelement = document.getElementById(`experience`) as HTMLInputElement;
+  const skillselement = document.getElementById(`skills`) as HTMLInputElement;
+
+  if (nameelement && emailelement && phoneelement && experienceelement && skillselement && educationelement) {
+    const name = nameelement.value;
+    const email = emailelement.value;
+    const phone = phoneelement.value;
+    const education = educationelement.value;
+    const experience = experienceelement.value;
+    const skill = skillselement.value;
+
+    //create resume output
+    const resumeoutput = `
+<h2>Resume</h2>
+<P><strong>Name:</strong> ${name} </p>
+<P><strong>email:</strong>${email}</p>
+<P><strong>Phone Number:</strong>${phone}</p>
+
+<h3>Education</h3>
+<P>${education}</p>
+
+<h3>Skills</h3>
+<P>${skill}</p>
+`
+
+    const resumeoutputelement = document.getElementById(`resumeoutput`)
+    if (resumeoutputelement) {
+      resumeoutputelement.innerHTML = resumeoutput
+    } else {
+      console.error(`The Resume Output Is Missing`)
+    }
+
+  }
+
+})
